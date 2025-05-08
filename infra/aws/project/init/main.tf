167c9ad803b881086fbef54ed813924afdd7fbc2
Denys Krytskyi <denys.krytskyi13@gmail.com>
2025-05-08 14:26:24 +0200

    Update workload module logic and improvements

--- infra/aws/project/init/main.tf
+++ infra/aws/project/init/main.tf
@@ -40,6 +40,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "main_bucket_lifecycle" {
     id     = "ExpireOldVersions"
     status = "Enabled"
 
+    filter {
+      prefix = ""
+    }
+
     noncurrent_version_expiration {
       noncurrent_days = 30
     }
