426cecac9e7aeddc9736e63fbde1406154170428
Denys Krytskyi <denys.krytskyi13@gmail.com>
2025-05-13 15:50:28 +0200

    adjusted GB Action env deploy_v5

--- infra/aws/env/stage/locals.tf
+++ infra/aws/env/stage/locals.tf
@@ -1,15 +1,11 @@
 locals {
-  env             = "stage" # Change this to the appropriate environment (e.g., "dev", "prod")
-  ami             = "ami-0e54671bdf3c8ed8d"
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  instance_name   = "${local.env}_instance"
-  instance_type   = "t2.micro"
-  key_name        = "${local.env}_deployer"
-  private_key     = "~/.ssh/${local.env}_deployer"
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
-  public_key      = file("~/.ssh/${local.env}_deployer.pub")
-  region          = "eu-central-1"
-  vpc_cidr        = "10.1.0.0/16"
-  vpc_name        = "${local.env}_vpc"
+  env           = "stage" # Change this to the appropriate environment (e.g., "dev", "prod")
+  az            = "eu-central-1a"
+  instance_name = "${local.env}_instance"
+  instance_type = "t2.micro"
+  key_name      = "${local.env}_deployer"
+  public_subnet = "10.1.101.0/24"
+  region        = "eu-central-1"
+  vpc_cidr      = "10.1.0.0/16"
+  vpc_name      = "${local.env}_vpc"
 }
--- infra/aws/env/stage/main.tf
+++ infra/aws/env/stage/main.tf
@@ -1,16 +1,15 @@
 module "stage_ec2" {
-  source              = "../../../../modules/aws/env_setup"
-  ami                 = local.ami
-  azs                 = local.azs
+  source = "git::https://github.com/didistars13/terraform.git//modules/aws/env_setup?ref=v1.0.7"
+  #source              = "../../../../modules/aws/env_setup"
+  az                  = local.az
   env                 = local.env
   instance_type       = local.instance_type
   instance_name       = local.instance_name
   key_name            = local.key_name
   allowed_cidr_blocks = var.allowed_cidr_blocks
-  private_key         = local.private_key
-  private_subnets     = local.private_subnets
-  public_key          = local.public_key
-  public_subnets      = local.public_subnets
+  private_key         = var.private_key
+  public_key          = var.public_key
+  public_subnet       = local.public_subnet
   region              = local.region
   vpc_cidr            = local.vpc_cidr
   vpc_name            = local.vpc_name
